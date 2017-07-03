873fcacecd30be1469b012e8ce99f80df5822217
Cody Rocker <Cody.Rocker.83@gmail.com>
2017-07-03 01:29:02 -0400

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Author: cody@code-sandbox.net 2017
 import sys
 import urllib
 import urllib2
@@ -53,7 +52,26 @@ class Log:
     def error(tag, message, error_code=-1, exit=False):
         error_output = _log_wrapper % ('error' if VERBOSE else 'E')
         print(error_output.format(tag=tag, msg=message))
-        if exit: sys.exit(error_code)    
+        if exit: sys.exit(error_code)
+
+class UserAgent:
+    """ Utility class for easy UserAgent selection
+    """
+    global UA_LIST
+
+    @staticmethod
+    def get_random():
+        """ Return a (pseudo)random User-Agent from UA_LIST
+        """
+        if len(UA_LIST) == 1: return UA_LIST[0]
+        elif len(UA_LIST) > 1:
+            selected_ua = UA_LIST[random.randint(0, len(UA_LIST) - 1)]
+            Log.debug('UserAgent.get_random', '"{}"'.format(
+                selected_ua if len(selected_ua) <= 64 else \
+                selected_ua[:61] + '...'))
+            return selected_ua
+        else:
+            Log.error('UserAgent.get_random', 'Empty user_agent list', exit=True)
 
 class Entity(object):
     ''' This object holds the necessary data for a media item 
@@ -61,22 +79,22 @@ class Entity(object):
     '''
 
     def __repr__(self):
-        """ Simple string representation
+        """ Formal string representation
         """
-        return '\n{ \n\ttitle: %s, \n\tsrc: %s, \n\tgenre: %s, \n\tinfo: %s, \n\tthumbnail: %s, \n\tfanart: %s \n}' % (
+        return '\n{\n\ttitle: %s,\n\tsrc: %s,\n\tgenre: %s,\n\tinfo: %s,\n\tthumbnail: %s,\n\tfanart: %s\n}' % (
             self.title, self.src, self.genre, self.info, self.thumbnail, self.fanart)
 
     def __str__(self):
-        """ Formal string representation
+        """ Simple string representation
         """
         return '{:24}: <{}/watch?v={}>'.format(
             self.title if len(self.title) <= 24 else self.title[:21] + '...', 
             BASE_URL, self.src)
 
     def __eq__(self, obj):
-        if self.src == obj.src:
-            return True
-        return False
+        """ Ensure unique content in set generation
+        """
+        return True if self.src == obj.src else False
 
     def create(self, item_dict):
         """ create assumes this dictionary is correctly formatted and all
@@ -96,23 +114,6 @@ class Entity(object):
     def to_xml(self):
         pass
 
-class UserAgent:
-    global UA_LIST
-
-    @staticmethod
-    def get_random():
-        """ Return a (pseudo)random User-Agent from UA_LIST
-        """
-        if len(UA_LIST) == 1: return UA_LIST[0]
-        elif len(UA_LIST) > 1:
-            selected_ua = UA_LIST[random.randint(0, len(UA_LIST) - 1)]
-            Log.debug('UserAgent.get_random', '"{}"'.format(
-                selected_ua if len(selected_ua) <= 64 else \
-                selected_ua[:61] + '...'))
-            return selected_ua
-        else:
-            Log.error('UserAgent.get_random', 'Empty user_agent list', exit=True)
-
 #########################################################
 ##    Function Definitions
 #########################################################
@@ -121,26 +122,17 @@ def parse_args():
     """ Parse command line arguments and return a dict containting script
         parameters
     """
-    global DEBUG
-    global VERBOSE
-
-    args = {'query': None}
+    args = {'debug': DEBUG, 'verbose': VERBOSE, 'query': None}
+    
     for argument in sys.argv:
-
-        if argument in ['-d', '--debug']:
-            DEBUG = True
-            args['debug'] = True
-            Log.debug('parse_args', 'debug output enabled')
-
-        if argument in ['-vv', '--verbose']:
-            DEBUG = True
-            VERBOSE = True
-            args['verbose'] = True
-            Log.debug('parse_args', 'verbose output enabled')
+        if argument in ['-d', '--debug']: args['debug'] = True
+        if argument in ['-vv', '--verbose']: args['verbose'] = True
 
         if argument.startswith('-q=') or argument.startswith('--query='):
             tmp = argument.split('=')[1].replace('"', '')
             args['query'] = urllib.quote_plus(tmp)
+
+    Log.verbose('parse_args', 'args={}'.format(args))
 
     return args
 
@@ -180,6 +172,17 @@ def parse_entity(video_soup):
         'info': '; '.join([_.text.encode('utf-8') for _ in tmp_info])})
 
 
+def parse_videos_to_list(video_soup):
+    """ Parse an arbitrary set of html elements containing children of class
+        'yt-lockup-content' to a list of unique Entities
+    """
+    videos = []
+    for video in video_soup.find_all('div', class_='yt-lockup-content'):
+        videos.append(parse_entity(video))
+
+    return set(videos)
+
+
 def parse_feed_to_dict(feed, _dict=None):
     """ Create or append an html entity with class 'feed-item-dismissable' to
         a dictionary
@@ -190,12 +193,7 @@ def parse_feed_to_dict(feed, _dict=None):
     # grab the section title to use as dictionary key
     section_label = feed.find('span', class_='branded-page-module-title-text')
 
-    videos = []
-    for video in feed.find_all('div', class_='yt-lockup-content'):
-        videos.append(parse_entity(video))
-
-    videos = set(videos)
-    results[section_label.text] = videos
+    results[section_label.text] = parse_videos_to_list(feed)
 
     Log.debug('parse_feed_to_dict', '{} scraped successfully'.format(
         section_label.text) if len(videos) == 15 \
@@ -233,11 +231,7 @@ def scrape_query_results(query):
 
     query_results = soup.find('ol', class_='item-section')
 
-    videos = []
-    for video in query_results.find_all('div', class_='yt-lockup-content'):
-        videos.append(parse_entity(video))
-
-    return {'Search Results': set(videos)}
+    return {'Search Results': parse_videos_to_list(query_results)}
 
 
 def show_results(results):
@@ -261,19 +255,25 @@ def show_results(results):
 #########################################################
 
 if __name__ == '__main__':
-    args = parse_args()  # check command line arguments to set script parameters
+    args = parse_args()  # check command line arguments ...
+    
+    # ... to set script parameters
+    DEBUG = args['debug'] or args['verbose']
+    if DEBUG: Log.debug(__name__, 'debug output enabled')
+    VERBOSE = args['verbose']
+    if VERBOSE: Log.debug(__name__, 'verbose output enabled')
+
+    query = args['query']
 
     # perform script operation
-    if args['query'] is not None:
-        results = scrape_query_results(args['query'])
-    else:
-        results = scrape_homepage()
+    show_results(
+        scrape_homepage() if query is None else \
+        scrape_query_results(query)
+        )
 
-    # # at this point we should have a 'results' dictionary along the lines of:
-    # # {
-    # #   'Section A': [<Entity.a1>, <Entity.a2>, ...],
-    # #   'Section B': [<Entity.b1>, <Entity.b2>, ...],
-    # #   ...
-    # # }
-
-    show_results(results)
+    # at this point we should have a 'results' dictionary along the lines of:
+    # {
+    #   'Section A': [<Entity.a1>, <Entity.a2>, ...],
+    #   'Section B': [<Entity.b1>, <Entity.b2>, ...],
+    #   ...
+    # }
